72177c7fc4bed1abf881e44769d8a6884d969ecc
Leiya Kenney <leiya.kenney@gmail.com>
2019-08-12 05:38:19 -0600

    Completes task_manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,7 +19,25 @@ class TasksController < ApplicationController
   end
 
   def show
-    binding.pry
     @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update({
+      title: params[:task][:title],
+      description: params[:task][:description]
+      })
+    task.save
+    redirect_to "/tasks/#{task.id}"
+  end
+
+  def destroy
+    Task.destroy(params[:id])
+    redirect_to '/tasks'
   end
 end
